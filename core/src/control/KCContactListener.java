0972898700e4b7f08bf13e05e6179b02a3336edc
krkn0001 <kristoffer_knutsson@hotmail.com>
2015-04-21 16:20:56 +0200

    Småändringar, icke fungerande kod
    Temporärt många fel i koden, ytterliggare commit att vänta

--- core/src/control/KCContactListener.java
+++ core/src/control/KCContactListener.java
@@ -1,6 +1,8 @@
 package control;
 
 import models.KCVars;
+import models.player.Player;
+import models.spell.Spell;
 
 import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.ContactImpulse;
@@ -16,8 +18,6 @@ public class KCContactListener implements ContactListener {
 	 * action.
 	 */
 	
-	KCInput kc = KCInput.getInstance();
-	
 	
 	@Override
 	public void beginContact(Contact contact) {
@@ -26,7 +26,7 @@ public class KCContactListener implements ContactListener {
 		if(fa.getUserData() != null && fb.getUserData() != null){
 			System.out.println("fa: " + fa.getUserData().toString());
 			System.out.println("fb: " + fb.getUserData().toString());
-			if(KCInput.){
+			if(){
 				KCVars.playerIsInLava = true;
 			}else if((fa.getUserData().equals("spell") || fb.getUserData().equals("spell"))
 					&&
@@ -72,6 +72,61 @@ public class KCContactListener implements ContactListener {
 		
 	}
 
+	public static KCInput getInstance(){
+		if(kc == null){
+			kc = new KCInput();
+		}
+		return kc;
+	}
+	
+	private boolean isPlayerSpellCollision(Fixture fa, Fixture fb){
+		Object oa = fa.getUserData();
+		Object ob = fb.getUserData();
+		
+		if((oa instanceof Player && ob instanceof Spell)
+				||
+			(oa instanceof Spell && ob instanceof Player)){
+			return true;
+		}else return false;
+	}
+	
+	private boolean isPlayerLavaCollision(Fixture fa, Fixture fb){
+		Object oa = fa.getUserData();
+		Object ob = fb.getUserData();
+		
+		if(oa.equals("lava")){
+			return ob instanceof Player; 
+		}else if(ob.equals("lava")){
+			return oa instanceof Player;
+		}else return false;
+	}
+	private boolean isSpellWorldWallCollision(Fixture fa, Fixture fb){
+		Object oa = fa.getUserData();
+		Object ob = fb.getUserData();
+		
+		if(oa.equals("world wall")){ //Unsure what this should be equals to, since it's not yet created
+			return ob instanceof Spell;
+		}else if(ob.equals("world wall")){
+			return oa instanceof Player;
+		}else return false;
+	}
+	
+	private boolean isSpellObstacleCollsion(Fixture fa, Fixture fb){
+		Object oa = fa.getUserData();
+		Object ob = fb.getUserData();
+		
+		if(oa.equals("obstacle")){ //Same goes here, this might end up as an object
+			return ob instanceof Spell;
+		}else if(ob.equals("obstale")){
+			return oa instanceof Spell;
+		}else return false;
+		
+		
+	}
+	
+	
+	
+	
 	
 
 }
--- core/src/control/KCInput.java
+++ core/src/control/KCInput.java
@@ -12,67 +12,10 @@ public class KCInput {
 	 * A help class for our input processor.
 	 * Will decide what kind of bodies that is colliding
 	 */
-	private static KCInput kc;
 	
 //	public static boolean goUp = false;
 //	public static boolean goDown = false;
 //	public static boolean goLeft = false;
 //	public static boolean goRight = false;
-	
-	public static KCInput getInstance(){
-		if(kc == null){
-			kc = new KCInput();
-		}
-		return kc;
-	}
-	
-	public boolean isPlayerSpellCollision(Fixture fa, Fixture fb){
-		Object oa = fa.getUserData();
-		Object ob = fb.getUserData();
-		
-		if((oa instanceof Player && ob instanceof Spell)
-				||
-			(oa instanceof Spell && ob instanceof Player)){
-			return true;
-		}else return false;
-	}
-	
-	public boolean isPlayerLavaCollision(Fixture fa, Fixture fb){
-		Object oa = fa.getUserData();
-		Object ob = fb.getUserData();
-		
-		if(oa.equals("lava")){
-			return ob instanceof Player; 
-		}else if(ob.equals("lava")){
-			return oa instanceof Player;
-		}else return false;
-	}
-	public boolean isSpellWorldWallCollision(Fixture fa, Fixture fb){
-		Object oa = fa.getUserData();
-		Object ob = fb.getUserData();
-		
-		if(oa.equals("world wall")){ //Unsure what this should be equals to, since it's not yet created
-			return ob instanceof Spell;
-		}else if(ob.equals("world wall")){
-			return oa instanceof Player;
-		}else return false;
-	}
-	
-	public boolean isSpellObstacleCollsion(Fixture fa, Fixture fb){
-		Object oa = fa.getUserData();
-		Object ob = fb.getUserData();
-		
-		if(oa.equals("obstacle")){ //Same goes here, this might end up as an object
-			return ob instanceof Spell;
-		}else if(ob.equals("obstale")){
-			return oa instanceof Spell;
-		}else return false;
-		
-		
-	}
-	
-	
-	
-	
 	
 }
