d7febff527a5f679d6708e415e0101bfd2a2a143
Wassburgare <daniel.wassbjer@gmail.com>
2015-04-26 12:22:53 +0200

    Added some stuff

--- core/src/view/BattleView.java
+++ core/src/view/BattleView.java
@@ -4,22 +4,31 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 
 import controller.KCConstants;
+import controller.gamestates.BattleState;
 
 public class BattleView {
-	Box2DDebugRenderer debugRenderer;
-	SpriteBatch spriteBatch;
-	OrthographicCamera worldCam, hudCam, box2DCam;
+	private SpriteBatch spriteBatch;
+	private OrthographicCamera worldCam, hudCam, box2DCam;
+	private Box2DDebugRenderer debugRenderer;
 	
-	PlayerView playerView = new PlayerView();
-	FireballView fireballView = new FireballView();
+	private BattleState battleState;
 	
-	public BattleView() {
+	private OrthogonalTiledMapRenderer mapRenderer;
+	
+	private PlayerView playerView = new PlayerView();
+	private FireballView fireballView = new FireballView();
+	
+	public BattleView(BattleState battleState) {
+		this.battleState = battleState;
+		
 		spriteBatch = new SpriteBatch();
 		
 		debugRenderer = new Box2DDebugRenderer();
+		mapRenderer = new OrthogonalTiledMapRenderer(battleState.getTiledMap());
 		
 		// Cameras
 		worldCam = new OrthographicCamera(KCConstants.GAME_WIDTH, KCConstants.GAME_HEIGHT);
@@ -40,6 +49,14 @@ public class BattleView {
 		// Clear screen
 		Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
+		// Render the map
+		mapRenderer.setView(worldCam);
+		mapRenderer.render();
 		
+		// Render objects on map
+		playerView.render();
+		fireballView.render();
+		
+		debugRenderer.render(battleState.getWorld(), box2DCam.combined);
 	}
 }
