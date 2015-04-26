30071086a947071259313ee9256374310b552678
Rektorn <Nalexdotk@gmail.com>
2015-04-26 16:37:37 +0200

    Added a new method to PlayerManager

--- core/src/controller/PlayerManager.java
+++ core/src/controller/PlayerManager.java
@@ -1,15 +1,21 @@
 package controller;
 
+import java.util.ArrayList;
 import java.util.List;
-import com.badlogic.gdx.Input.Keys;
 
 import model.player.Player;
+
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.physics.box2d.Body;
 
 public class PlayerManager {
 	
 	List<PlayerController> pControllers;
+	PlayerManager playerManager;
 	
 	public PlayerManager(List<Player> players){
+		pControllers = new ArrayList<PlayerController>();
+		
 		PlayerController p1 = new PlayerController(players.get(0),Keys.W ,Keys.S,Keys.A, Keys.D,Keys.Q,Keys.E);
 		PlayerController p2 = new PlayerController(players.get(1), Keys.U, Keys.J, Keys.H, Keys.K, Keys.Y, Keys.I);
 		PlayerController p3 = new PlayerController(players.get(2), Keys.UNKNOWN, Keys.UNKNOWN, Keys.UNKNOWN, Keys.UNKNOWN, Keys.UNKNOWN, Keys.UNKNOWN);
@@ -29,5 +35,13 @@ public class PlayerManager {
 	
 	public List<PlayerController> getPlayerControllers(){
 		return pControllers;
+	}
+	
+	public List<Body> getPlayerBodies(){
+		List<Body> bodies = new ArrayList<Body>();
+		for(PlayerController p: pControllers){
+			bodies.add(p.getBody());
+		}
+		return bodies;
 	}
 }
