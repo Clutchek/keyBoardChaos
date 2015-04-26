6f7e2dcc9b25ac14fd5cb1bb35e3b55bd387e654
Poya Kaboteh <poya.kaboteh@hotmail.com>
2015-04-26 13:23:07 +0200

    Started on FireballView

--- core/src/view/FireballView.java
+++ core/src/view/FireballView.java
@@ -1,5 +1,45 @@
 package view;
 
-public class FireballView {
+import model.main.KeyboardChaosModel;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Disposable;
+
+public class FireballView implements Disposable {
+		
+	private SpriteBatch spriteBatch;
+
+	private World world;
+	private BattleView battleView;
+	
+	private BodyDef bDef;
+	private Body body;
+	private FixtureDef fDef;
+	private Fixture fixture;
+
+	
+
+	public FireballView (BodyDef bDef, Body body, int xPos, int yPos){
+		this.bDef = bDef;
+		this.body = body;
+		
+		this.bDef.type = BodyType.KinematicBody;
+		this.bDef.position.set(xPos, yPos);
+		
+		fDef = new FixtureDef();
+		
+		
+	}
+	
+	@Override
+	public void dispose() {
+		
+	}
 
 }
