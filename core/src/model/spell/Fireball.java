45bc8cf354cb841f2582943246a8d6bd2369e07d
Poya Kaboteh <poya.kaboteh@hotmail.com>
2015-05-11 13:52:28 +0200

    did stuff in fireball

--- core/src/model/spell/Fireball.java
+++ core/src/model/spell/Fireball.java
@@ -2,17 +2,25 @@ package model.spell;
 
 import model.player.Player;
 
+import com.badlogic.gdx.math.Vector2;
+
+import controller.spellcontroller.FireballController;
+
 public class Fireball implements Spell{
 	
 	private int damage, projectileSpeed;
 	private Player originPlayer;
+	private Fireball fireball;
 	private final float fireballRadius;
+	private FireballController fireballController;
 
 	
 	public Fireball(Player originPlayer){
 		this.damage = 10;
 		this.projectileSpeed = 1;
 		this.originPlayer = originPlayer;
+		this.fireball = new Fireball(originPlayer);
+		this.fireballController = new FireballController(this, originPlayer);
 		fireballRadius = 3f;
 		
 	}
@@ -41,6 +49,18 @@ public class Fireball implements Spell{
 	
 	public float getFireballRadius(){
 		return fireballRadius;
+	}
+	
+	public Vector2 getVector(){
+		
+	}
+	
+	public float getPosX(){
+		return fireballController.getBody().getPosition().x;
+	}
+	
+	public float getPosY(){
+		return fireballController.getBody().getPosition().y;
 	}
 
 }
