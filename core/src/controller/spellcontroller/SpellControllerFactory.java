da33d2d5e1387b658978c42680060cd1b2cb632e
Wassburgare <daniel.wassbjer@gmail.com>
2015-05-13 19:57:02 +0200

    Replace old arrows with new ones. Fix the spawn point of fireball relative to player position

--- core/src/controller/spellcontroller/SpellControllerFactory.java
+++ core/src/controller/spellcontroller/SpellControllerFactory.java
@@ -3,6 +3,7 @@ package controller.spellcontroller;
 import model.player.Player;
 import model.spell.Fireball;
 import model.spell.Spell;
+import controller.KCConstants;
 import controller.body.FixtureManager;
 
 public class SpellControllerFactory {
@@ -17,8 +18,17 @@ public class SpellControllerFactory {
 		//kan behöva kolla efter null
 		if(spell instanceof Fireball){
 			Fireball fireball = new Fireball();
-			fireball.setPosX(player.getPosX() + player.getVector().getX()*10);
-			fireball.setPosY(player.getPosY() + player.getVector().getY()*10);
+			float spaceX = player.getVector().getX()*(player.getSize()+fireball.getFireballRadius());
+			float spaceY = player.getVector().getY()*(player.getSize()+fireball.getFireballRadius());
+			if (player.getVector().getX() != 0 && player.getVector().getY() != 0) {
+				spaceX *= Math.sqrt(2)/2;
+				spaceY *= Math.sqrt(2)/2;
+			}
+			float posX = player.getPosX() + spaceX;
+			float posY = player.getPosY() + spaceY;
+			
+			fireball.setPosX(posX);
+			fireball.setPosY(posY);
 			return new FireballController(fireball, player, fixtureManager);
 		}
 		return null;
