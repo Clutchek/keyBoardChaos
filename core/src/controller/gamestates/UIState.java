87c62e223b9b2fe50e8b86401383326da1f4c33e
Poya Kaboteh <poya.kaboteh@hotmail.com>
2015-05-20 17:23:51 +0200

    added stuff

--- core/src/controller/gamestates/UIState.java
+++ core/src/controller/gamestates/UIState.java
@@ -1,7 +1,12 @@
 package controller.gamestates;
 
+import model.gui.StartMenu;
+
 public class UIState implements GameState {
 
+	private StartMenu startMenu;
+	private StartMenuView startMenuView;
+	
 	@Override
 	public void update() {
 		// TODO Auto-generated method stub
