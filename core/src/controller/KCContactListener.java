351dce7183d2bb3fb23a3d4625d2a558e5ad9e2c
krkn0001 <kristoffer_knutsson@hotmail.com>
2015-05-24 11:34:54 +0200

    Snyggat till lite i ContactListener
    Den behöver fortfarande ge ut lava skada

--- core/src/controller/KCContactListener.java
+++ core/src/controller/KCContactListener.java
@@ -32,6 +32,25 @@ public class KCContactListener implements ContactListener {
 	
 	@Override
 	public void beginContact(Contact contact) {
+		if(isLavaInContact(contact) && isPlayerInContact(contact)){
+			PlayerController pc = getPlayerFromContact(contact);
+			
+		
+		}else if(isTwoSpellsInContact(contact)){
+			fixtureManager.addToDisposeList(contact.getFixtureA());
+			fixtureManager.addToDisposeList(contact.getFixtureB());
+		
+		}else if(isPlayerInContact(contact) && isSpellInContact(contact)){
+			Spell spell = getSpellFromContact(contact);
+			fixtureManager.addToDisposeList(getSpellFixture(contact));
+						
+			PlayerController pc = getPlayerFromContact(contact);
+
+			if(spell instanceof Fireball){
+				Fireball fb = (Fireball)spell;
+				pc.getPlayer().setHealthPoints(pc.getPlayer().getHealthPoints() - fb.getDamage());
+			}
+		}
 		
 	}
 
@@ -51,67 +70,15 @@ public class KCContactListener implements ContactListener {
 		// TODO Auto-generated method stub
 		
 	}
-	
-	private void handleContact(Contact c){
-		Fixture fixA = c.getFixtureA();
-		Fixture fixB = c.getFixtureB();
 		
-		
-	}
-	
-	private void determineContactType(Contact c){
-		Object fixtureAData;
-		Object fixtureBData;
-		
-		Fixture fixA = c.getFixtureA();
-		Fixture fixB = c.getFixtureB();
-		
-		//************************************NULL CHECK*************************************// Remove?
-		if(fixA.getUserData() != null && fixB.getUserData() != null){
-			System.out.println("Some fixtures user data is null");
-		//***********************************************************************************//
-			
-			//Checks if the contacts contain of one player fixture and one spell fixture
-			if((fixA.getUserData() instanceof Spell || fixA.getUserData() instanceof PlayerController)
-					&& (fixB.getUserData() instanceof Spell || fixB.getUserData() instanceof PlayerController)){
-				Spell spell = getSpellFromContact(c);
-				Fireball fb = (Fireball)spell;
-				PlayerController pc = getPlayerFromContact(c);
-				
-				pc.getPlayer().setHealthPoints(pc.getPlayer().getHealthPoints() - fb.getDamage()); //Deal damage to player
-				
-				//This can be done prettier - Adds a spell fixture to the dispose list since it has hit something...
-				if(fixA.getUserData() instanceof Spell){
-					fixtureManager.addToDisposeList(fixA);
-				}else if(fixB.getUserData() instanceof Spell){
-					fixtureManager.addToDisposeList(fixB);
-				}
-				
-				
-			
-			//Checks if the contacts are two spell fixtures	
-			}else if(fixA.getUserData() instanceof Spell && fixB.getUserData() instanceof Spell){
-				fixtureManager.addToDisposeList(fixA);
-				fixtureManager.addToDisposeList(fixB);
-				
-			//Checks if the contact is between a player fixture and lava	
-			}else if((fixA.getUserData() instanceof PlayerController || fixA.getUserData().equals("lava")
-					&& (fixB.getUserData() instanceof PlayerController || fixB.getUserData().equals("lava")))){
-				/*
-				 * So, here we need some algorithm to dish out damage to the player as long as this contact is maintained
-				 */
-			}
-			
-		}
-		
-	}
-	
 	private PlayerController getPlayerFromContact(Contact c){
+		Fixture fixA = c.getFixtureA();
+		Fixture fixB = c.getFixtureB();
 		
-		if(c.getFixtureA().getUserData() instanceof PlayerController){
-			return (PlayerController)c.getFixtureA().getUserData();
-		}else if(c.getFixtureB().getUserData() instanceof PlayerController){
-			return (PlayerController)c.getFixtureB().getUserData();
+		if(fixA.getUserData() instanceof PlayerController){
+			return (PlayerController)(fixA.getUserData());
+		}else if(fixB.getUserData() instanceof PlayerController){
+			return (PlayerController)(fixB.getUserData());
 		}else
 			return null; 	//Denna metod ropas aldrig pÂ om man inte redan vet att en fixture i en contact ‰r en player
 							//sÂ null borde aldrig bli returnerat. Fortfarande inte snyggt med null... Exception?
@@ -120,18 +87,64 @@ public class KCContactListener implements ContactListener {
 	
 	
 	private Spell getSpellFromContact(Contact c){
+		Fixture fixA = c.getFixtureA();
+		Fixture fixB = c.getFixtureB();
 		
-		if(c.getFixtureA().getUserData() instanceof Spell){
-			return (Spell)c.getFixtureA().getUserData();
-		}else if(c.getFixtureB().getUserData() instanceof Spell){
-			return (Spell)c.getFixtureB().getUserData();
+		if(fixA.getUserData() instanceof Spell){
+			return (Spell)(fixA.getUserData());
+		}else if(fixB.getUserData() instanceof Spell){
+			return (Spell)(fixB.getUserData());
 		}else
 			return null; 	//Denna metod ropas aldrig pÂ om man inte redan vet att en fixture i en contact ‰r en spell
 							//sÂ null borde aldrig bli returnerat. Fortfarande inte snyggt med null... Exception?
 				
 	}
 	
-
+	private boolean isLavaInContact(Contact c){
+		Fixture fixA = c.getFixtureA();
+		Fixture fixB = c.getFixtureB();
+		if (fixA.getUserData().equals("lava") || fixB.getUserData().equals("lava")){
+			return true;
+		}else
+			return false;
+	}
 	
+	private boolean isPlayerInContact(Contact c){
+		Fixture fixA = c.getFixtureA();
+		Fixture fixB = c.getFixtureB();
+		if(fixA.getUserData() instanceof PlayerController || fixB.getUserData() instanceof PlayerController){
+			return true;
+		}else
+			return false;
+	}
+	
+	private boolean isTwoSpellsInContact(Contact c){
+		Fixture fixA = c.getFixtureA();
+		Fixture fixB = c.getFixtureB();
+		if((fixA.getUserData() instanceof Spell) && (fixB.getUserData() instanceof Spell)){
+			return true;
+		}else
+			return false;
+	}
+
+	private boolean isSpellInContact(Contact c){
+		Fixture fixA = c.getFixtureA();
+		Fixture fixB = c.getFixtureB();
+		if((fixA.getUserData() instanceof Spell) || (fixB.getUserData() instanceof Spell)){
+			return true;
+		}else
+			return false;
+	}
+	
+	private Fixture getSpellFixture(Contact c){
+		Fixture fixA = c.getFixtureA();
+		Fixture fixB = c.getFixtureB();
+		if(fixA.getUserData() instanceof Spell){
+			return fixA;
+		}else if(fixB.getUserData() instanceof Spell){
+			return fixB;
+		}else
+			return null;
+	}
 	
 }
