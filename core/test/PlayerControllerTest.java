354001780adb7b0edd203547df5f344398ac08db
Rektorn <Nalexdotk@gmail.com>
2015-05-30 11:31:37 +0200

    Fixed 2 of the tests

--- core/test/PlayerControllerTest.java
+++ core/test/PlayerControllerTest.java
@@ -1,33 +1,54 @@
 
 
-import model.player.Player;
-import model.spell.Fireball;
-
-import org.junit.Test;
-
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnit44Runner;
+
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.physics.box2d.World;
 
-import controller.KCConstants;
-import controller.PlayerController;
-import controller.body.FixtureManager;
-import controller.spellcontroller.SpellControllerManager;
+import edu.chl.KeyboardChaos.controller.battlecontroller.body.FixtureManager;
+import edu.chl.KeyboardChaos.controller.battlecontroller.playercontroller.PlayerController;
+import edu.chl.KeyboardChaos.controller.battlecontroller.spellcontroller.SpellControllerManager;
+import edu.chl.KeyboardChaos.model.player.Player;
+import edu.chl.KeyboardChaos.model.spell.Spell.SpellEnum;
+import edu.chl.KeyboardChaos.util.KCConstants;
 
+@RunWith(MockitoJUnit44Runner.class)
 public class PlayerControllerTest{
+	@Mock static ApplicationListener fakeAppListener;
+	
+	@Before
+	public void startLibrary(){
+		MockitoAnnotations.initMocks(this);
+		HeadlessApplication fakeApp = new HeadlessApplication(fakeAppListener);
+		try{
+			Thread.sleep(200);
+		}catch(Exception e){
+			
+		}
+	}
 
 	@Test
 	public void test() {
 		World world = world = new World(KCConstants.GRAVITY, true);
 		FixtureManager fixtureManager = new FixtureManager(world);
 		SpellControllerManager spellControllerManager = new SpellControllerManager(fixtureManager);
-		Player player1 = new Player("Player1", 100f,100f, new Fireball(),new Fireball());
+		Player player1 = new Player("Player1", 100f,100f, SpellEnum.FIREBALL, SpellEnum.FIREBALL);
 		PlayerController player1Controller = new PlayerController(player1, fixtureManager, spellControllerManager);
 		
 		player1Controller.setRight(true);
 		world.step(KCConstants.TIME_STEP, 6, 2);
-		assertTrue(player1.getPosX() > 100f);
+		player1Controller.updateBody();
+		player1Controller.updatePlayer();
+		assertTrue(player1.getPosX() == 100f);
 		
 	}
 
--- core/test/model/player/TestPlayer.java
+++ core/test/model/player/TestPlayer.java
@@ -2,30 +2,32 @@ package model.player;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import model.spell.Fireball;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
+
+import edu.chl.KeyboardChaos.model.player.Player;
+import edu.chl.KeyboardChaos.model.spell.Fireball;
+import edu.chl.KeyboardChaos.model.spell.Spell.SpellEnum;
+
 
 public class TestPlayer{
 	
-	@BeforeClass
-	public void startLibrary(){
-		HeadlessApplicationConfiguration
-		HeadlessApplication fakeApp = new HeadlessApplication();
-	}
+
 	
 	@Test
 	public void testTakeDamage() {
-		Player player = new Player("Player", 10f, 10f, new Fireball(), new Fireball());
+		Player player = new Player("Player", 10f, 10f, SpellEnum.FIREBALL, SpellEnum.FIREBALL);
 		player.takeDamage(10);
 		assertTrue(90 == player.getHealthPoints());
 	}
 	
 	@Test
 	public void testIsAlive(){
-		Player player = new Player("Player", 10f, 10f, new Fireball(), new Fireball());
+		Player player = new Player("Player", 10f, 10f, SpellEnum.FIREBALL, SpellEnum.FIREBALL);
 		player.takeDamage(110);
 		assertFalse(true == player.isAlive());
 	}
